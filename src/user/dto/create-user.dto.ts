b06a82fa64599d39afce0e036d637a56612668f9
Kwonyujin04 <2310867@sookmyung.ac.kr>
2025-07-26 23:31:14 +0900

    remove: remove venv #14

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+// src/users/dto/create-user.dto.ts
+import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    readonly email: string;
+
+    @IsString()
+    @MinLength(8)
+    readonly password: string;
+}
