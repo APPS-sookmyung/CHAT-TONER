c9301e0abc711f93177d54ef796db4182eaa6527
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-10-10 13:16:13 +0900

    feat/#98: add SVG as React component support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import tailwind from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [react(), tailwind(), svgr()],
   server: {
     proxy: {
       "/api": {
